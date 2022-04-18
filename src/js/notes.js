0167f02ffe8a09385ded7a448e40994c38504dbd
Oksana Stezhko <oksana_stegko@ukr.net>
2022-04-19 00:06:52 +0300

    закончила редактирование, часть архивирования

--- src/js/notes.js
+++ src/js/notes.js
@@ -1,19 +1,26 @@
 import notesTemplate from '../templates/notes.hbs';
 import moment from 'moment';
+
 const re = /[0-1]?[0-9][-\.\/][0-3]?[0-9][-\.\/][12][09][0-9][0-9]/g;
-const renderNotes = function (arr) {
+
+const formatedNotes = function (arr) {
   const formatedArr = arr.map(elem => {
     const formatedDate = moment(elem.created).locale('en').format('MMM DD, YYYY');
     const arrDates = elem.content.match(re);
     const selectionDates = !!arrDates ? arrDates.join(', ') : '';
     return { ...elem, created: formatedDate, dates: selectionDates };
   });
+  return formatedArr;
+};
+
+const renderNotes = function (arr, sign) {
+  const filteredArr = arr.filter(elem => String(elem.archived) === sign);
+  const formatedArr = formatedNotes(filteredArr);
   const markup = notesTemplate(formatedArr);
+
   const notesRef = document.querySelector('.js-notes');
   notesRef.innerHTML = '';
   notesRef.insertAdjacentHTML('beforeend', markup);
 };
-
-// const addNote = function () {};
 
 export { renderNotes };
